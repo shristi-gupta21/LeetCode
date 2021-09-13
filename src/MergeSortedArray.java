b0537da1e59b54361ac7277768846b3c695a0e0d
shristi-gupta21 <shristi.gupta_cs19Agla.ac.in>
2021-09-13 22:59:55 +0530

    Some updations  done

--- src/MergeSortedArray.java
+++ src/MergeSortedArray.java
@@ -1,3 +1,33 @@
-public class MergeSortedArrray {
+import java.util.Arrays;
 
+public class MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int nums1length = m+n;
+        int j = 0;
+        if (nums2.length == 0){
+            System.out.println(Arrays.toString(nums1));
+        }
+        else if(m == 0 && n==1){
+            nums1[0] = nums1[0] + nums2[0];
+            System.out.println(Arrays.toString(nums1));
+        }else{
+            for (int i = m; i < nums1length ; i++) {
+                nums1[i] = nums1[i] + nums2[j];
+                j++;
+            }
+
+            Arrays.sort(nums1);
+            System.out.println(Arrays.toString(nums1));
+        }
+
+    }
+            
+
+    public static void main(String[] args) {
+        MergeSortedArray mergeSortedArray = new MergeSortedArray();
+        int[] nums1 = {1};
+        int[] nums2 = {};
+        mergeSortedArray.merge(nums1,1,nums2,0);
+
+    }
 }
